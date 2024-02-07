aeb811afeed0e7f1ab1c3950a02f30f4fe407fe8
yeasinshakil <shakilrock1998@gmail.com>
2024-02-07 19:20:46 +0600

    creating skeleton

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,30 @@
 import DropzoneComponent from "@/components/Dropzone";
+import { db } from "@/firebase";
+import { FileType } from "@/typings";
 import { auth } from "@clerk/nextjs";
+import { collection, getDocs } from "firebase/firestore";
 
-const page = () => {
-  const {userId} = auth()
-  return <div>
-    <DropzoneComponent />
-  </div>;
+const page = async () => {
+  const { userId } = auth()
+  const docResults = await getDocs(collection(db, 'users', userId!, 'files'))
+
+  const skeletonFile: FileType[] = docResults.docs.map((doc) => ({
+    id: doc.id,
+    fileName: doc.data().fileName || doc.id,
+    fullName: doc.data().fullName,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    downloadUrl: doc.data().downloadUrl,
+    type: doc.data().type,
+    size: doc.data().size
+  }))
+
+  console.log(skeletonFile)
+
+  return (
+    <div>
+      <DropzoneComponent />
+    </div>
+  );
 };
 
 export default page;
---
+++ typings.d.ts
@@ -0,0 +1,9 @@
+export type FileType = {
+    id: string,
+    fileName: string,
+    fullName: string,
+    downloadUrl: string,
+    timestamp: Date,
+    type: string,
+    size: number,
+}
